d30f83d4e1e7b02ca7a6e628818633bffed7e8d7
WarotAsawa <warot.asawa@gmail.com>
2024-03-14 09:12:55 +0700

    Update help and source reference

--- KBSearch.py
+++ KBSearch.py
@@ -3,9 +3,19 @@ import json
 import sys
 
 class KBSearch:
-    def Retrieve(self,client, knowledgeID, text):
-        response = client.retrieve(
-            knowledgeBaseId=knowledgeID,
+    
+    def get_ssm_parameter(self,parameter_name):
+        try:
+            response = self.ssm_client.get_parameter(Name=parameter_name)
+            return response['Parameter']['Value']
+        except Exception as e:
+            print(f"Error retrieving SSM parameter {parameter_name}: {e}")
+            return None
+        return None
+        
+    def Retrieve(self, text):
+        response = self.agentRuntimeClient.retrieve(
+            knowledgeBaseId=self.knowledgeID,
             retrievalQuery={
                 'text': text
             },
@@ -19,6 +29,7 @@ class KBSearch:
     
     def RetrieveAndGenerate(self,text):
         response = self.agentRuntimeClient.retrieve_and_generate(
+            #sessionId='string',
             input={
                 'text': text
             },
@@ -29,16 +40,21 @@ class KBSearch:
                     'modelArn': self.modelArn
                 }
             }
+            #,sessionConfiguration={
+            #    'kmsKeyArn': 'string'
+            #}
         )
+        #print(response['retrievedReferences'])
         return response
     
     def GetKMID(self,client):
         response = client.list_knowledge_bases()
         return response
     
-    def GetFMID(self, client):
-        response = client.list_feature_groups()
-        return response
+    def ListKMID(self):
+        client = boto3.client('bedrock', region_name='us-east-1')
+        response = client.list_foundation_models(byInferenceType='ON_DEMAND')
+        return response['modelSummaries']
 
     def TranslateToThai(self, text):
         response = self.translateClient.translate_text(
@@ -57,10 +73,10 @@ class KBSearch:
         return response.get('TranslatedText')
 
     def __init__(self, knowledgeID, modelArn):
-
         self.knowledgeID = knowledgeID
         self.modelArn = modelArn
-        self.agentRuntimeClient = boto3.client('bedrock-agent-runtime')
+        self.agentRuntimeClient = boto3.client('bedrock-agent-runtime', region_name='us-east-1')
         self.translateClient = boto3.client(service_name='translate', region_name='us-east-1', use_ssl=True)
-            
-        
+        self.ssm_client = boto3.client('ssm', region_name='us-east-1')
+
+        #agentClient = boto3.client('bedrock-agent')
--- chatbot_app.py
+++ chatbot_app.py
@@ -1,12 +1,12 @@
 import streamlit as st #all streamlit commands will be available through the "st" alias
 from KBSearch import KBSearch
 
-st.set_page_config(page_title="Warot Chatbot") #HTML title
-st.title("Warot Chatbot") #page title
-# Select your BedRock KM ID
-kmID = 'your-km-id'
-# Select your BedRock FM ARN
-modelArn = "arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-v2"
+st.set_page_config(page_title="Sri Trang Agro Industry KB Chatbot") #HTML title
+st.title("Sri Trang Agro Industry KB ChatBot") #page title
+st.text("See prompt example here:", help="Amazon BedRock คืออะไร , AWS public IPv4 address มีกี่ประเภท , EC2 มีกี่ Instance Type , AWS Certified Data Engineer มีค่าสอบเท่าไหร่")
+
+kmID = 'R6D2NW0H7N'
+modelArn = "arn:aws:bedrock:us-east-1::foundation-model/anthropic.claude-instant-v1"
 searcher = KBSearch(kmID, modelArn)
 
 if modelArn != "":
@@ -14,18 +14,18 @@ if modelArn != "":
     modelName = ":gray[Now using model :] *:orange[" + modelName +"]*"
     st.markdown(modelName)
 
+
 if 'chat_history' not in st.session_state: #see if the chat history hasn't been created yet
     st.session_state.chat_history = [] #initialize the chat history
 
-
-
 for message in st.session_state.chat_history: #loop through the chat history
     with st.chat_message(message["role"]): #renders a chat line for the given role, containing everything in the with block
-        st.markdown(message["text"]) #display the chat content
+        if "help" in message: st.markdown(message["text"], help=message["help"]) #display the chat content
+        else: st.markdown(message["text"]) #display the chat content
 
 
 inputText = st.chat_input("Chat with your bot here") #display a chat input box
-
+searcher.Retrieve("text")
 if inputText: #run the code in this if block after the user submits a chat message
     
     with st.chat_message("user"): #display a user chat message
@@ -36,11 +36,29 @@ if inputText: #run the code in this if block after the user submits a chat messa
     thaiInput = thaiResponse.get('TranslatedText')
     sourceLan = thaiResponse.get('SourceLanguageCode')
     print(sourceLan)
-    modelResponse = searcher.RetrieveAndGenerate(thaiInput) #call the model through the supporting library
-    chatResponse = searcher.TranslateFromThai(modelResponse,sourceLan)
-    with st.chat_message("assistant"): #display a bot chat message
-        st.markdown(chatResponse) #display bot's latest response
+    modelResponse = searcher.RetrieveAndGenerate(thaiInput, kmID, modelArn) #call the model through the supporting library
+    textResponse = modelResponse['output']['text']#call the model through the supporting library
+    # Get all referenced source from the Citations
+    sourceHelp = ""
+    sourceCount = 0
+    citations = modelResponse['citations']#
+    for citation in citations:
+        retrievedReferences = citation['retrievedReferences']
+        for ref in retrievedReferences:
+            sourceCount = sourceCount + 1;
+            sourceHelp += "Source " + str(sourceCount) + " 🗃️ : \n"
+            sourceHelp += "["+str(ref['location']['s3Location']['uri'])+"]("+str(ref['location']['s3Location']['uri'])+")\n\n"
+            sourceHelp += "Refered text:\n```\n"
+            sourceHelp += str(ref['content']['text']) + "\n"
+            sourceHelp += "```\n\n"
     
-    st.session_state.chat_history.append({"role":"assistant", "text":chatResponse}) #append the bot's latest message to the chat history
-
-
+    # Translate back
+    chatResponse = searcher.TranslateFromThai(textResponse,sourceLan)
+    with st.chat_message("assistant"): #display a bot chat message
+        if sourceHelp == "":
+            st.markdown(chatResponse) #display bot's latest response
+            st.session_state.chat_history.append({"role":"assistant", "text":chatResponse}) #append the bot's latest message to the chat history
+        else:
+            sourceHelp = "**Refered from :green["+str(sourceCount)+"] sources:**\n\n"+sourceHelp
+            st.markdown(chatResponse, help=sourceHelp) #display bot's latest response
+            st.session_state.chat_history.append({"role":"assistant", "text":chatResponse, "help":sourceHelp}) #append the bot's latest message to the chat history
